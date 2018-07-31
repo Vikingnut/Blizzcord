63e418e5a9ccae41c1fae06c721bb0a207bae402
Garrett Chesnut <garrettdc2@gmail.com>
2018-07-30 21:38:09 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+
+    if (message.content == 'kms') {
+        message.channel.sendMessage('Call 1-800-273-8255');
+    } else if (message.content == 'ping') {
+        message.channel.sendMessage('pong');
+    } else if (message.content == 'Omae wa mou shindeiru') {
+        message.channel.sendMessage('NANI??!');
+    }
+});
+
+bot.login('NDczNjQ2MTgwNTIyNTI0Njcy.DkE-GQ.9L864jyCIMQ3vZJbcgCPuNBM430');
